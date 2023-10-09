dda48e4a34451f34c2b7c5a291b8d5e49bbf19b3
xrsQugi <02.04.5.art@gmail.com>
2023-10-09 23:32:46 +0300

    first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  gallery.insertAdjacentHTML(
+    "afterbegin",
+    `<li class="gallery__item">
+            <a class="gallery__link" href=${galleryItem.original}>
+                <img
+                    class="gallery__image"
+                    src=${galleryItem.preview}
+                    data-source=${galleryItem.original}
+                    alt=${galleryItem.description}
+                />
+            </a>
+        </li>`
+  );
+});
 
 console.log(galleryItems);
