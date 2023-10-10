4c09d38d7c1f6f8c3f01352b011d3dcd9ee4345a
xrsQugi <02.04.5.art@gmail.com>
2023-10-10 10:31:02 +0300

    FINALLY

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const gallery = document.querySelector(".gallery");
+gallery.addEventListener('click', onImgClick);
 
 galleryItems.forEach((galleryItem) => {
   gallery.insertAdjacentHTML(
@@ -19,3 +21,29 @@ galleryItems.forEach((galleryItem) => {
 });
 
 console.log(galleryItems);
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
+}
